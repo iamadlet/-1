e02275be0d6c81fbb9d53d1427c1fd54013fbcdc
iamadlet <adlet.zhumagaliev2003@gmail.com>
2025-10-30 14:02:30 +0500

    Initial Commit

---
+++ FirstTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstTest
+//
+//  Created by Адлет Жумагалиев on 30.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
